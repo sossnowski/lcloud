4c9d1974ce072b7f37aae76df9aed3a19c45312f
sossnowski <kbrzoska97@gmail.com>
2019-11-26 12:46:20 +0100

    upload file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const aws = require('aws-sdk');
 require('dotenv').config();
+const fs = require('fs');
 
 
-work = async () => {
+listingFiles = async () => {
     try {
         aws.config.setPromisesDependency();
         aws.config.update({
@@ -15,7 +16,9 @@ work = async () => {
         const result = await s3.listObjectsV2({
             Bucket: 'lcloud-427-ts',
         }).promise();
-        console.log(result);
+        for (let i = 0; i < result.Contents.length; i++) {
+            console.log(result.Contents[i])
+        }
     } catch (error) {
         console.log(error);
     }
@@ -23,5 +26,35 @@ work = async () => {
 
 }
 
-work();
+const uploadFile = (fileName) => {
+    try {
+        aws.config.setPromisesDependency();
+        aws.config.update({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            region: 'eu-west-2'
+        })
+        const fileContent = fs.readFileSync(fileName);
+
+        const params = {
+            Bucket: 'lcloud-427-ts',
+            Key: 'uploadedFile.txt',
+            Body: fileContent
+        };
+        const s3 = new aws.S3();
+
+        s3.upload(params, function(err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log(`File uploaded successfully. ${data.Location}`);
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//listingFiles();
+uploadFile('./fileToUpload.txt')
 
